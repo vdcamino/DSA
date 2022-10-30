387029cf7a833602e00c6b6b767c27ec3a1ccee7
victordacamino <victordacamino@gmail.com>
2022-10-30 02:14:26 +0100

    ...

--- Patterns/OOP/Design_Parking_Lot.py
+++ Patterns/OOP/Design_Parking_Lot.py
@@ -1,177 +1,185 @@
-import time
 from enum import Enum
+import time
 
 
-class TicketStatus(enum.Enum):
-    ACTIVE = 1
-    COMPLETE = 2
+class ParkingTicketStatus(Enum):
+    PAID = 1
+    UNPAID = 2
 
 
-class VehicleType(enum.Enum):
-    CAR = 1
-    BIKE = 2
-
-
-class SpotType(enum.Enum):
+class ParkingSpaceStatus(Enum):
     FREE = 1
     TAKEN = 2
 
 
-class ParkingLot:
-    def __init__(self, name, address):
-        self.name = name
-        self.address = address
-        self.level = []
-
-    def addLevel(self, floor):
-        self.level.append(floor)
-
-    def processEntry(self, ticket):
-        for eachlevel in self.level:
-            if eachlevel.spots[ticket.veh.type][SpotType.FREE]:
-                ticket.spot = eachlevel.assignSpot(ticket)
-                print("Entry Completed For : ", ticket.veh.num)
-                break
-
-    def processExit(self, ticket):
-        for eachlevel in self.level:
-            if ticket.spot in eachlevel.spots[ticket.veh.type][SpotType.TAKEN]:
-                eachlevel.unassignSpot(ticket)
-                break
-        ticket.outTime = time.time()
-        ticket.spots = None
-        ticket.status = TicketStatus.COMPLETE
-        ticket.payment = Payment(ticket.inTime, ticket.outTime)
-        print(
-            "Exit Completed For  : ",
-            ticket.veh.num,
-            " Pay : ",
-            int(ticket.payment.amount),
-        )
+class VehicleSize(Enum):
+    SMALL = 1
+    MEDIUM = 2
+    LARGE = 3
 
 
-class ParkingLevel:
-    def __init__(self, name):
-        self.name = name
-        self.spots = {
-            VehicleType.CAR: {SpotType.FREE: [], SpotType.TAKEN: []},
-            VehicleType.BIKE: {SpotType.FREE: [], SpotType.TAKEN: []},
-        }
+class PaymentType(Enum):
+    CARD = 1
+    CASH = 2
 
-    def assignSpot(self, ticket):
-        if self.spots[ticket.veh.type][SpotType.FREE] != []:
-            spot = self.spots[ticket.veh.type][SpotType.FREE].pop()
-            ticket.spot = spot
-            self.spots[ticket.veh.type][SpotType.TAKEN].append(spot)
-            return ticket.spot
-        return False
 
-    def unassignSpot(self, ticket):
-        self.spots[ticket.veh.type][SpotType.FREE].append(ticket.spot)
-        self.spots[ticket.veh.type][SpotType.TAKEN].remove(ticket.spot)
+class ParkingSpaceType(Enum):
+    BIKE_PARKING = 1
+    CAR_PARKING = 2
+    TRUCK_PARKING = 3
 
-    def addSpot(self, type, num):
-        for eachnum in range(num):
-            spot = Spot(type)
-            self.spots[type][SpotType.FREE].append(spot)
+
+class PaymentStatus(Enum):
+    PENDING = 1
+    SUCCESS = 2
+    CANCELLED = 3
+    REFUNDED = 4
+    UNPAID = 5
 
 
 class Vehicle:
-    def __init__(self, num):
-        self.id = self.generateID()
-        self.num = num
-
-    def generateID(self):
-        yield range(100)
-
-
-class Car(Vehicle):
-    def __init__(self, num):
-        super().__init__(num)
-        self.type = VehicleType.CAR
+    def __init__(self):
+        self.type = None
+        self.plate = None
+        self.parkingTicket = None
+        self.paymentInfo = None
 
 
-class Bike(Vehicle):
-    def __init__(self, num):
-        super().__init__(num)
-        self.type = VehicleType.CAR
+class ParkingLot:
+    def __init__(self, address, name):
+        self.parkingFloors = []
+        self.entrances = []
+        self.exits = []
+        self.address = address
+        self.name = name
+
+        def isParkingSporAvailableForVehicle(self, vehicle: Vehicle):
+            return None
+
+        def updateParkingAttendant(
+            self, parkingAttendant: ParkingAttendant, gateId: int
+        ):
+            return None
 
 
-class Spot:
-    def __init__(self, type):
-        self.id = self.generateID()
-        self.type = type
+class ParkingFloor:
+    def __init__(self):
+        self.levelId = None
+        self.isFull = False
+        self.parkingSpaces = []
+        self.parkingDisplayBoard = None
 
-    def generateID(self):
-        yield range(100)
+
+class ParkingTicket:
+    def __init__(self):
+        self.entryTime = time.time()
+        self.exitTime = None
+        self.levelId = None
+        self.spotId = None
+        self.parkingSpaceType = None
+        self.totalCost = 0
+        self.parkingTicketStatus = None
+
+        def updateTotalCost(self):
+            return None
+
+        def updateVehicleExitTime(self, vehicleExitDateTime):
+            return None
+
+
+class Gate:
+    def __init__(self):
+        self.gateId = None
+        self.parkingAttendant = None
+
+
+class Entrance(Gate):
+    def __init__(self):
+        super().__init__()
+
+    def getParkingTicket(self, vehicle: Vehicle):
+        # return ParkingTicket object based on vehicle type
+        return None
+
+
+class Exit(Gate):
+    def __init__(self):
+        super().__init__()
+
+    def payForParking(self, parkingTicket: ParkingTicket):
+        # return ParkingTicket object updated (all necessary information is contained in the parkingTicket object)
+        return None
+
+
+class Address:
+    def __init__(self):
+        self.country = None
+        self.state = None
+        self.street = None
+        self.zipCode = None
+
+
+class ParkingSpace(Enum):
+    def __init__(self):
+        self.id = None
+        self.isFree = True
+        self.type = None
+
+
+class ParkingDisplayBoard:
+    def __init__(self):
+        self.spotsAvailablePerFloor = {}
+
+    def updateFreeSpotsAvailable(self, parkingSpace: ParkingSpace):
+        return None
+
+
+class Account:
+    def __init__(self):
+        self.name = None
+        self.email = None
+        self.password = None
+        self.phone = None
+        self.address = None
+
+
+class Admin(Account):
+    def __init__(self):
+        super().__init__()
+
+    def addParkingFloor(self, parkingLot: ParkingLot, parkingFloor: ParkingFloor):
+        return None
+
+    def addParkingSpace(self, parkingFloor: ParkingFloor, parlingSpace: ParkingSpace):
+        return None
+
+    def addParkingDisplayBoard(
+        self, parkingFloor: ParkingFloor, parkingDisplayBoard: ParkingDisplayBoard
+    ):
+        return None
+
+
+class ParkingAttendant(Account):
+    def __init__(self):
+        super().__init__()
+        self.gateId = None
+
+    def processVehicleEntry(self, vehicle: Vehicle, entry: Entrance):
+        return None
+
+    def processPayment(self, parkingTicket: ParkingTicket, exit: Exit):
+        return None
 
 
 class Payment:
-    def __init__(self, inTime, outTime):
-        self.mode = None
-        self.rate = [30, 20, 10]
-        self.amount = self.calAmount(inTime, outTime)
-
-    def getRate(self):
-        return self.rate
-
-    def setRate(self, rate):
-        self.rate = rate
-
-    def calAmount(self, inTime, outTime):
-        amount = (outTime - inTime) * self.getRate()[0]
-        amount += (
-            (outTime - inTime - 60) * self.getRate()[1]
-            if outTime - inTime - 60 > 0
-            else 0
-        )
-        amount += (
-            (outTime - inTime - 120) * self.getRate()[2]
-            if outTime - inTime - 120 > 0
-            else 0
-        )
-        return amount
+    def makePayment(self, ticket: ParkingTicket, paymentType: PaymentType):
+        return None
 
 
-class Ticket:
-    def __init__(self, veh):
-        self.veh = veh
-        self.status = TicketStatus.ACTIVE
-        self.inTime = time.time()
-        self.outTime = None
-        self.payment = None
-        self.spot = None
-
-    def generateID(self):
-        # some ID generation mechanism
-        return ID
-
-
-class DisplayBoard:
-    def show(self, P):
-        for eachlevel in P.level:
-            print(P.name, "-", eachlevel.name, "- Available Parking Spots")
-            print("Car  : ", len(eachlevel.spots[VehicleType.CAR][SpotType.FREE]))
-            print("Bike : ", len(eachlevel.spots[VehicleType.BIKE][SpotType.FREE]))
-
-
-P = ParkingLot("Google Parking Lot", "123, Fort, Mumbai")
-F1 = ParkingLevel("F1")
-F1.addSpot(VehicleType.CAR, 3)
-F1.addSpot(VehicleType.BIKE, 3)
-P.addLevel(F1)
-
-Board = DisplayBoard()
-Board.show(P)
-
-T1 = Ticket(Car("MH05 AB 5454"))
-P.processEntry(T1)
-
-T2 = Ticket(Bike("MH05 AB 9000"))
-P.processEntry(T2)
-
-time.sleep(2)
-P.processExit(T2)
-
-Board = DisplayBoard()
-Board.show(P)
+class PaymentInfo:
+    def __init__(self):
+        self.amount = None
+        self.type = None
+        self.date = None
+        self.parkingTicket = None
+        self.status = None
