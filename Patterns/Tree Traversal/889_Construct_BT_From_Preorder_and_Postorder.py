de1a83d103f44a7aedad2e458e1b7fe024d49725
victordacamino <victordacamino@gmail.com>
2022-11-09 00:50:03 +0100

    ...

---
+++ Patterns/Tree Traversal/889_Construct_BT_From_Preorder_and_Postorder.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    preIndex, postIndex = 0, 0
+
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]):
+        # Nested helper function
+        def buildTreeNode():
+            root = TreeNode(preorder[self.preIndex])
+            self.preIndex += 1
+            if root.val != postorder[self.postIndex]:
+                root.left = buildTreeNode()
+            if root.val != postorder[self.postIndex]:
+                root.right = buildTreeNode()
+            self.postIndex += 1
+            return root
+
+        return buildTreeNode()
