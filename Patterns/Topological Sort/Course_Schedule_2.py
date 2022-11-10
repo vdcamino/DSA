b6859131843437fdd0e7811dbf6c589ee905380b
victordacamino <victordacamino@gmail.com>
2022-11-10 21:07:27 +0100

    ...

--- Patterns/Topological Sort/Course_Schedule_2.py
+++ Patterns/Topological Sort/Course_Schedule_2.py
@@ -1,35 +1,30 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # topological sort
-        # - identify the nodes with 0 indegree (nodes that have no incoming edge/no prerequisites)
-        # - traverse the graph starting from each one of these nodes
-        # - if cycle detected, return False
-        # - if len(visited) == numCourses: return True, else False
-
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # create graph (adj_list) + hashmap to keep track of the number of incoming edges for each node
         graph = {i: [] for i in range(numCourses)}
         in_degree = {i: 0 for i in range(numCourses)}
 
-        # build graph and indegree hashmap
+        # build edges + indegree
         for course, prereq in prerequisites:
             graph[prereq].append(course)
             in_degree[course] += 1
 
-        # get nodes with no incoming edges
+        # build a list containing only nodes with no incoming edges
         nodes_in_degree_zero = []
         for course in graph:
             if in_degree[course] == 0:
                 nodes_in_degree_zero.append(course)
 
-        # variable to keep track of the visited nodes; hashset is not necessary for this problem
-        visited = 0
+        # topological sort list keeping the visiting order
+        top_sort = list()
 
         # traverse the graph starting from the nodes with no incoming edges
         while nodes_in_degree_zero:
             cur_node = nodes_in_degree_zero.pop()
-            visited += 1
+            top_sort.append(cur_node)
             for neighbor in graph[cur_node]:
                 in_degree[neighbor] -= 1
                 if in_degree[neighbor] == 0:
                     nodes_in_degree_zero.append(neighbor)
 
-        return visited == numCourses
+        return top_sort if len(top_sort) == numCourses else []
