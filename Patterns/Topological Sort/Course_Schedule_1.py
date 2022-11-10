f03dd25240497149d901fed95e31c739c954543e
victordacamino <victordacamino@gmail.com>
2022-11-10 20:08:00 +0100

    ...

--- Patterns/Topological Sort/Course_Schedule_1.py
+++ Patterns/Topological Sort/Course_Schedule_1.py
@@ -3,38 +3,38 @@ class Solution:
         # topological sort
         # - identify the nodes with 0 indegree (nodes that have no incoming edge/no prerequisites)
         # - traverse the graph starting from each one of these nodes
-        # - if cycle detected, return false
-        # - if len(visited) == numCourses: return True
+        # - if cycle detected, return False
+        # - if len(visited) == numCourses: return True, else False
 
         graph = {i: [] for i in range(numCourses)}
         indegree = {i: 0 for i in range(numCourses)}
 
         # build graph and indegree hashmap
-        for prereq in prerequisites:
-            graph[prereq[1]].append(prereq[0])
-            indegree[prereq[0]] += 1
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+            indegree[course] += 1
 
         # get nodes with no incoming edges
-        nodes_with_no_incoming_edges = []
-        for node in graph:
-            if indegree[node] == 0:
-                nodes_with_no_incoming_edges.append(node)
+        nodes_in_degree_zero = []
+        for course in graph:
+            if indegree[course] == 0:
+                nodes_in_degree_zero.append(course)
 
         # hashset to keep track of the nodes we have visited
         visited = set()
 
         # traverse the graph starting from the nodes with no incoming edges
-        while len(nodes_with_no_incoming_edges) > 0:
-            node = nodes_with_no_incoming_edges.pop()
-            visited.add(node)
-            for neighbour in graph[node]:
+        while nodes_in_degree_zero:
+            cur_node = nodes_in_degree_zero.pop()
+            visited.add(cur_node)
+            for neighbor in graph[cur_node]:
                 # check for cycles
-                if neighbour in visited:
+                if neighbor in visited:
                     return False
                 # remove edge
-                indegree[neighbour] -= 1
+                indegree[neighbor] -= 1
                 # mark node as visited if it has no more prerequisites
-                if indegree[neighbour] == 0:
-                    nodes_with_no_incoming_edges.append(neighbour)
+                if indegree[neighbor] == 0:
+                    nodes_in_degree_zero.append(neighbor)
 
         return len(visited) == numCourses
