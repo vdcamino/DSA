20b013c56f5020b0ff2cc0473241010902e40005
victordacamino <victordacamino@gmail.com>
2022-11-11 11:21:33 +0100

    ...

--- Patterns/Topological Sort/Topological_Sort_Implementation.py
+++ Patterns/Topological Sort/Topological_Sort_Implementation.py
@@ -1,57 +1,56 @@
 class TopologicalSort:
 
     # Notes
-    # indegree of a vertice in a graph = number of edges incident on this vertice (incoming edges
-    # graphs with cycles don't have a possible topological sort, because it creates an impossible set of constraints
+    # 1. In-degree of a vertice in a graph = number of edges incident on this vertice (incoming edges)
+    # 2. Graphs with cycles don't have a possible topological sort, because the cycle blocks the traversal
+    # (the node that starts the cycle never reaches an in-degree value of zero)
 
     # Algorithm
-    # - Identify a node with no incoming edges
-    # - Add that node to the ordering
-    # - Decrement the indegree of this node's neighbours
+    # - Identify nodes with no incoming edges
+    # - Add these nodes to the ordering
+    # - Decrement the in-degree of these nodes' neighbors
     # - Repeat
 
-    # How to identify a cycle
-    # - Keep a set containing the nodes that are part of the current path
-    # - If you revisit a node of this path, you are in a cycle --> raise exception
+    # How to identify a cycle?
+    # - Keep a set (or a simple variable) containing the nodes that are part of the current path
+    # - If at the end of the traversal you didn't visit all the nodes, there is a cycle in the graph
 
     def topological_sort(digraph):
-        # digraph (directed graph) is a dictionary:
+        # digraph (directed graph) represented as an adjacency list by a dictionary:
         #   key: a node
         # value: a set of adjacent neighboring nodes
 
-        # construct a dictionary mapping nodes to their
-        # indegrees
-        indegrees = {node: 0 for node in digraph}
+        # build a dictionary mapping each node to its in-degree value
+        in_degree = {node: 0 for node in digraph}
         for node in digraph:
             for neighbor in digraph[node]:
-                indegrees[neighbor] += 1
+                in_degree[neighbor] += 1
 
         # track nodes with no incoming edges
         nodes_with_no_incoming_edges = []
         for node in digraph:
-            if indegrees[node] == 0:
+            if in_degree[node] == 0:
                 nodes_with_no_incoming_edges.append(node)
 
-        # initially, no nodes in our ordering
-        topological_ordering = []
+        topological_sort = []
 
         # as long as there are nodes with no incoming edges
         # that can be added to the ordering
-        while len(nodes_with_no_incoming_edges) > 0:
+        while nodes_with_no_incoming_edges:
 
             # add one of those nodes to the ordering
             node = nodes_with_no_incoming_edges.pop()
-            topological_ordering.append(node)
+            topological_sort.append(node)
 
-            # decrement the indegree of that node's neighbors
+            # decrement the indegree of this node's neighbors
             for neighbor in digraph[node]:
-                indegrees[neighbor] -= 1
-                if indegrees[neighbor] == 0:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
                     nodes_with_no_incoming_edges.append(neighbor)
 
         # we've run out of nodes with no incoming edges
         # did we add all the nodes or find a cycle?
-        if len(topological_ordering) == len(digraph):
-            return topological_ordering  # got them all
+        if len(topological_sort) == len(digraph):
+            return topological_sort  # got them all
         else:
             raise Exception("Graph has a cycle! No topological ordering exists.")
